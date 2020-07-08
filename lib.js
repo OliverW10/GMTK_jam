35fe9ad79db45f6dd41f1c5750a5e35698c1921e
EthanN <eth.ecn@gmail.com>
2020-07-08 15:16:18 +1000

    Removed auto scaling canvas

--- lib.js
+++ lib.js
@@ -497,12 +497,3 @@ Array.prototype.equals = function (array) {
 }
 // Hide method from for-in loops
 Object.defineProperty(Array.prototype, "equals", {enumerable: false});
-
-function AABBCollision(x1,y1,w1,h1,x2,y2,w2,h2){
-    if (x1 < x2 + w2 && x1 + w1 > x2 && y1 < y2 + h2 && y1 + h1 > y2){
-        return true;
-    }else{
-        return false;
-    }
-        
-}
--- main.js
+++ main.js
@@ -1,14 +1,5 @@
 var bgalpha = 1;
 
-function updateCanvasDimensions(){
-    h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-	w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    h *= 0.95;
-    w *= 0.95;
-	scale = h/600;
-	canvas.height = 600*scale;
-	canvas.width = 800*scale;
-}
 function draw(){
     drawRect(0,0,w,h,"white",true,"white",bgalpha);
     
@@ -21,7 +12,6 @@ function update(){
 }
 
 function main(){
-    updateCanvasDimensions();
     update();
     draw();
 
