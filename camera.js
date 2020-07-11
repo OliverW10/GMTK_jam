b8f84fe511cd5454bd2cfb6b67f9acc0ee048ad6
EthanN <eth.ecn@gmail.com>
2020-07-11 13:04:51 +1000

    eh

--- camera.js
+++ camera.js
@@ -1,16 +1,15 @@
 class Camera{
     constructor(people){
-        this.locations = [];
+        this.locations = [[0.45,0.85,0.08,0.15,1],[0.34,0.6,0.3,0.25,2],[0.40,0.35,0.13,0.25,3],[0.64,0.75,0.2,0.15,4],[0.64,0.35,0.1,0.35,5],[0.24,0.65,0.1,0.15,1]];
         this.currentLocation = 0;
         this.state = "overview";
         this.people = people;
     }
     draw(rect){
-        this.locations = [[0.45,0.85,0.08,0.15,1],[0.34,0.6,0.3,0.25,2],[0.40,0.35,0.13,0.25,3],[0.64,0.75,0.2,0.15,4],[0.64,0.35,0.1,0.35,5],[0.24,0.65,0.1,0.15,1]];
         drawRect(rect[0],rect[1],rect[2],rect[3],"black",0,"",1);
         for(var x of this.locations){
-            var temp = [];
-            drawRect(rect[0]+rect[2]*x[0],rect[1]+rect[3]*x[1],rect[2]*x[2],rect[3]*x[3],"black",0,"white",1);
+            var temp = [rect[0]+rect[2]*x[0],rect[1]+rect[3]*x[1],rect[2]*x[2],rect[3]*x[3]];
+            drawRect(temp[0],temp[1],temp[2],temp[3],"black",0,"white",1);
         }
     }
 }
