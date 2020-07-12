25269151e2f317f53c5e0f5c91422e6747e7a0e3
OliverW10 <34265013+OliverW10@users.noreply.github.com>
2020-07-12 10:09:03 +1000

    added tutorial to html, added bubble kinda

--- tutorial.js
+++ tutorial.js
@@ -1,23 +1,33 @@
 
 var bubbleSprite = new spriteSheet("assets/bubble.png",800,600,25,0,0,800,600)
-
+bubbleSprite.addState("test", 0, 5);
 class Tutorial{
 	constructor(){
 		this.state = this.intro
 		this.texts = [""];
+		this.bubbleProgress = 0;
+		this.bubbleSpeed = 0.1;
+		this.bubbleGoal  = 0;
 	}
 	createBubble(){
-
+		this.bubbleGoal = 5;
 	}
 	removeBubble(){
-
+		this.bubbleGoal = 0;
 	}
 	execute(){
+		if(this.bubbleGoal > this.bubbleProgress){
+			this.bubbleProgress += this.bubbleSpeed;
+		}
+		if(this.bubbleGoal < this.bubbleProgress){
+			this.bubbleProgress -= this.bubbleSpeed;
+		}
 		this.drawBubble();
 		this.state();
 	}
 	drawBubble(){
-		bubbleSprite.draw()
+		bubbleSprite.sheetX = Math.floor(this.bubbleProgress*bubbleSprite.w);
+		bubbleSprite.draw();
 	}
 	intro(){
 	}
